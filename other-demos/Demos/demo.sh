b45356b1e677de71aec753ddfa2d9f7d7daff9b2
Andrew Bennett <Andrew.Bennett@bjss.com>
2020-09-25 14:52:16 +0100

    Update demo.sh script

--- other-demos/Demos/demo.sh
+++ other-demos/Demos/demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #cat Test.java
-#javac Test.java
-#javap -p -c Test.class 
+javac Test.java
+javap -p -c Test.class 
 LD_LIBRARY_PATH=/jdk/src/utils/hsdis/build/linux-amd64/ java -XX:+UnlockDiagnosticVMOptions -XX:CompileCommand=print,*Test.complexMath -XX:+LogCompilation Test 
 
 #-XX:LogFile=mylogfile.log Test
